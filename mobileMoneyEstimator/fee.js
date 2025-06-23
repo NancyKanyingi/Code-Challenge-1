548f3f5432464eb9bbfb009372c3e21e2e380212
Nancy <wangui.kanyingi2015@gmail.com>
2025-06-23 23:56:02 +0300

    Adds fee.js

--- mobileMoneyEstimator/fee.js
+++ mobileMoneyEstimator/fee.js
@@ -1,9 +1,36 @@
-const prompt = require("prompt-sync")
-let num = prompt(Number(Sending, KES))
-if(1.5%num<=10){
-    console.log(Kshs10)
-}else if(1.5%num>=70){
-    console.log(Kshs70)
-}else if(10<1.5%num<70){
-    console.log(1.5%num)
+
+function estimateTransactionFee ()
+{
+    const prompt = require("prompt-sync")();
+    let amountToSend = prompt(Number("Unatuma Ngapi? (KES):"))
+    let calculatedTransactionFee = 0.015 * amountToSend
+    let totalDebitAmount = amountToSend + calculatedTransactionFee
+
+    if(calculatedTransactionFee <= 10){
+        
+        console.log(`Sending KES ${amountToSend}:
+        Calculated Transaction Fee: KES 10
+        Total amount to be debited: KES ${amountToSend + 10}
+
+        Send Money Securely!`)
+    }
+    else if(calculatedTransactionFee >= 70){
+        
+        console.log(`Sending KES ${amountToSend}:
+        Calculated Transaction Fee: KES 70
+        Total amount to be debited: KES ${amountToSend + 70}
+
+        Send Money Securely!`)
+    }
+    else if(10 < calculatedTransactionFee < 70){
+        
+        console.log(`Sending KES ${amountToSend}:
+        Calculated Transaction Fee: KES ${calculatedTransactionFee}
+        Total amount to be debited: KES ${totalDebitAmount}
+
+        Send Money Securely!`)
+    }
+
 }
+
+estimateTransactionFee()
