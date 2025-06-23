443d4a85c87aaf57dced2b8606109a1b96532e1f
Nancy <wangui.kanyingi2015@gmail.com>
2025-06-23 23:17:22 +0300

    Adds fare.js

--- bodaFareCalculator/fare.js
+++ bodaFareCalculator/fare.js
@@ -1,14 +1,15 @@
 const prompt  = require("prompt-sync")();
 
-let baseFareInKm = prompt(Number("Unafika wapi Mkubwa? Kilometer ngapi?"))
-let _distance  = prompt(Number("ukoWote? Io ni"))
-let _standardFare = ukikaliaPikipiki=50
-let _destination = mpakaUko=(15 * baseFareInKm)
-let _fare =Total=(50 + (15 * baseFareInKm))
+let distanceInKm = prompt(Number("Unafika wapi Mkubwa? Kilometer ngapi?"))
+let baseFare = 50;
+let chargePerKm = 15;
 
-console.log(`distance:${_distance},
-    standardFare:${_standardFare},
-    destination:${_destination},
-    fare:${_fare}
+let totalFare = baseFare + (chargePerKm * distanceInKm)
+
+console.log(`Uko kwote? Io ni ${distanceInKm} km:
+    Ukikalia Pikipiki: KES ${baseFare},
+    Mpaka Uko: KES ${chargePerKm * distanceInKm},
+    Total: KES ${totalFare}
 
 "Panda Pikipiki!"`)
+
